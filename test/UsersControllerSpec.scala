1beca395de452932359aee1fe9bbc28a35f12f35
Paul-Lavery <paul.lavery@unit9.com>
2016-06-28 17:03:02 +0100

    Added tests for Users controller

--- test/UsersControllerSpec.scala
+++ test/UsersControllerSpec.scala
@@ -3,7 +3,7 @@ package test
 import java.util.UUID
 import scala.concurrent.Future
 import play.api.Play.current
-import play.api.mvc.Result
+import play.api.mvc.{Result, AnyContentAsEmpty}
 import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json._
@@ -24,7 +24,8 @@ class UsersControllerSpec extends PlaySpec with ConfiguredApp {
   lazy val reactiveMongoApi = current.injector.instanceOf[ReactiveMongoApi]
 
   def userCollection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("users")
-  
+  lazy val controller = new Users(reactiveMongoApi)
+
   "User API" must {
 
     "add a new user" in {
@@ -35,7 +36,6 @@ class UsersControllerSpec extends PlaySpec with ConfiguredApp {
         "j.doe@aol.com",
         3
       )
-      val controller = new Users(reactiveMongoApi)
       val request = FakeRequest(
         method = "POST",
         uri = controllers.routes.Users.addUser.url,
@@ -52,6 +52,93 @@ class UsersControllerSpec extends PlaySpec with ConfiguredApp {
       whenReady(userFuture) { usr => usr mustBe Some(user) }
     }
 
+    "not add a user twice" in {
+      val user = User(
+        UUID.fromString("00000000-0000-0000-0000-000000000001"), 
+        "John", 
+        "Doe", 
+        "j.doe@aol.com",
+        3
+      )
+      val request = FakeRequest(
+        method = "POST",
+        uri = controllers.routes.Users.addUser.url,
+        headers = FakeHeaders(),
+        body = Json.toJson(user)
+      )
+      val apiResult: Future[Result] = controller.addUser.apply(request)
+      val jsonResult: JsValue = contentAsJson(apiResult)
+      jsonResult mustNot be (null)
+      (jsonResult \ "status").as[String] mustBe ("KO")
+      (jsonResult \ "message").as[String] mustBe ("An object with same ID already exists")
+    }
+
+    "get a user" in {
+      val userID = "00000000-0000-0000-0000-000000000001"
+      val request = FakeRequest(
+        method = "GET",
+        uri = s"/user/get/$userID",
+        headers = FakeHeaders(),
+        body = AnyContentAsEmpty
+      )
+      val apiResult: Future[Result] = controller.getUser(userID).apply(request)
+      val jsonResult: JsValue = contentAsJson(apiResult)
+      jsonResult mustNot be (null)
+      val user: Option[User] = jsonResult.validate[User].asOpt 
+      user mustNot be (None)
+      val userFuture = userCollection
+          .find(Json.obj("_id" -> user.get._id))
+          .one[User]
+      whenReady(userFuture) { usr => usr mustBe user }
+    }
+
+   "delete a user" in {
+      val userID = "00000000-0000-0000-0000-000000000001"
+      val request = FakeRequest(
+        method = "POST",
+        uri = controllers.routes.Users.deleteUser(userID).url,
+        headers = FakeHeaders(),
+        body = AnyContentAsEmpty
+      )
+      val apiResult: Future[Result] = controller.deleteUser(userID).apply(request)
+      val jsonResult: JsValue = contentAsJson(apiResult)
+      jsonResult mustNot be (null)
+      (jsonResult \ "status").as[String] mustBe ("OK")
+      val userFuture = userCollection
+          .find(Json.obj("_id" -> userID))
+          .one[User]
+      whenReady(userFuture) { usr => usr mustBe None }
+    }
+
+    "reject incorrect JSON" in {
+      val badJSON = Json.obj()
+      val request = FakeRequest(
+        method = "POST",
+        uri = controllers.routes.Users.addUser.url,
+        headers = FakeHeaders(),
+        body = badJSON
+      )
+      val apiResult: Future[Result] = controller.addUser.apply(request)
+      val jsonResult: JsValue = contentAsJson(apiResult)
+      jsonResult mustNot be (null)
+      (jsonResult \ "status").as[String] mustBe ("KO")
+      (jsonResult \ "message").as[String] mustBe ("Invalid JSON request")
+    }
+
+    "reject incorrect UUID" in {
+      val badUUID = "001"
+      val request = FakeRequest(
+        method = "GET",
+        uri = s"/user/get/$badUUID",
+        headers = FakeHeaders(),
+        body = AnyContentAsEmpty
+      )
+      val apiResult: Future[Result] = controller.getUser(badUUID).apply(request)
+      val jsonResult: JsValue = contentAsJson(apiResult)
+      jsonResult mustNot be (null)
+      (jsonResult \ "status").as[String] mustBe ("KO")
+      (jsonResult \ "message").as[String] mustBe ("Invalid UUID format")
+    }
 
   }
 
